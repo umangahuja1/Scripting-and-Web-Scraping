8d8d2a3ce811870a4c635e2922fc6c2128e03841
Umang Ahuja <umangahuja1@gmail.com>
2017-10-28 17:58:42 +0000

    Create following_count.py

---
+++ Twitter/following_count.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import requests,sys
+
+#you can also get the twitter handle from user
+handle = input('Enter twitter handle to check followers : ') 
+res = requests.get('https://twitter.com/'+handle)
+soup = BeautifulSoup(res.text,'lxml')
+
+try:
+    follow_box = soup.find('li',{'class':'ProfileNav-item ProfileNav-item--following'})
+    following = follow_box.find('a').find('span',{'class':'ProfileNav-value'})
+    print("Number of people {} is following are {}: ".format(handle,following.get('data-count')))
+
+except:
+    print('Cannot find the handle right now')
+    sys.exit()
