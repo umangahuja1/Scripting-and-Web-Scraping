41b245e9941a58c41ca5465382b40ef7f43e9c25
Umang Ahuja <umangahuja1@gmail.com>
2017-10-28 18:14:07 +0000

    Update following_count.py

--- Twitter/following_count.py
+++ Twitter/following_count.py
@@ -1,15 +1,15 @@
 from bs4 import BeautifulSoup
-import requests,sys
+import requests
 
 handle = input('Enter twitter handle : ') 
 res = requests.get('https://twitter.com/'+handle)
 soup = BeautifulSoup(res.text,'lxml')
 
 try:
-    follow_box = soup.find('li',{'class':'ProfileNav-item ProfileNav-item--following'})
+    following_box = soup.find('li',{'class':'ProfileNav-item ProfileNav-item--following'})
     following = follow_box.find('a').find('span',{'class':'ProfileNav-value'})
     print("Number of people {} is following are {}: ".format(handle,following.get('data-count')))
 
 except:
     print('Cannot find the handle right now')
-    sys.exit()
+ 
