7a48c80fd371537a648e8a01b3e3deab6607397e
Umang Ahuja <umangahuja1@gmail.com>
2017-10-28 18:17:21 +0000

    Update favorites_count.py

--- Twitter/favorites_count.py
+++ Twitter/favorites_count.py
@@ -1,15 +1,14 @@
 from bs4 import BeautifulSoup
-import requests,sys
+import requests
 
 handle = input('Enter twitter handle to check favorite/liked post of a user : ') 
 res = requests.get('https://twitter.com/'+handle)
 soup = BeautifulSoup(res.text,'lxml')
 
 try:
-    follow_box = soup.find('li',{'class':'ProfileNav-item ProfileNav-item--favorites'})
-    favorite = follow_box.find('a').find('span',{'class':'ProfileNav-value'})
+    favorite_box = soup.find('li',{'class':'ProfileNav-item ProfileNav-item--favorites'})
+    favorite = favorite_box.find('a').find('span',{'class':'ProfileNav-value'})
     print("Number of post {}  liked are {}: ".format(handle,favorite.get('data-count')))
 
 except:
     print('Cannot find the handle right now')
-    sys.exit()
