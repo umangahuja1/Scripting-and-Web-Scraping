87da80b4d16accbef008327167f7f3c7ea20e914
Umang Ahuja <umangahuja1@gmail.com>
2017-10-28 18:01:52 +0000

    Create favorites_count.py

---
+++ Twitter/favorites_count.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import requests,sys
+
+#you can also get the twitter handle from user
+handle = input('Enter twitter handle to check followers : ') 
+res = requests.get('https://twitter.com/'+handle)
+soup = BeautifulSoup(res.text,'lxml')
+
+try:
+    follow_box = soup.find('li',{'class':'ProfileNav-item ProfileNav-item--favorites'})
+    favorite = follow_box.find('a').find('span',{'class':'ProfileNav-value'})
+    print("Number of post {}  liked are {}: ".format(handle,favorite.get('data-count')))
+
+except:
+    print('Cannot find the handle right now')
+    sys.exit()
